905825f5114b686a6d9209723f77cfe7e7b99c06
uael <lucas.abel@yandex.com>
2018-04-17 14:49:45 +0200

    fucking history (wip)

--- lib/taskmaster.rb
+++ lib/taskmaster.rb
@@ -1,12 +1,29 @@
+require 'readline'
+
 require 'taskmaster/version'
 require 'taskmaster/reader'
 require 'taskmaster/config'
 
 module Taskmaster
   def self.main
+    begin
+      File.readlines(ENV['HOME'] + '/.tmst').each do |line|
+        Readline::HISTORY.push(line)
+      end
+    rescue Exception => ex
+      # ignored
+    end
+
     Taskmaster::Config.load
     loop do
       Taskmaster::Reader.getline
+    end
+
+    if (f = File.open(ENV['HOME'] + '/.tmst', 'w'))
+      Readline::HISTORY.each do |line|
+        f.puts line
+      end
+      f.close
     end
   end
 end
--- lib/taskmaster/quit.rb
+++ lib/taskmaster/quit.rb
@@ -2,6 +2,14 @@ require_relative 'proc'
 
 module Taskmaster
   def self.quit(_args)
+
+    if (f = File.open(ENV['HOME'] + '/.tmst', 'w'))
+      Readline::HISTORY.each do |line|
+        f.write line + '\n'
+      end
+      f.close
+    end
+
     puts('kthxbye')
     exit
   end
